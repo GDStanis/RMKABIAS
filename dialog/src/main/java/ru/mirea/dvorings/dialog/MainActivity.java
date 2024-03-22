e8f12f4a3eea5c645d7a04bd620dba8a9cbed4cd
GDStanis <rlybruh@yandex.ru>
2024-03-23 02:09:11 +0300

    task complete

--- dialog/src/main/java/ru/mirea/dvorings/dialog/MainActivity.java
+++ dialog/src/main/java/ru/mirea/dvorings/dialog/MainActivity.java
@@ -3,12 +3,17 @@ package ru.mirea.dvorings.dialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener,
+        DatePickerDialog.OnDateSetListener {
 
     String LOG = "MainLifecycle";
 
@@ -64,5 +69,32 @@ public class MainActivity extends AppCompatActivity {
     public void onNeutralClicked() {
         Toast.makeText(getApplicationContext(), "Вы выбрали кнопку \"На паузе\"!",
                 Toast.LENGTH_LONG).show();
+    }
+
+    public void onClickTimeDialog(View view) {
+        MyTimeDialogFragment timeDialog = new MyTimeDialogFragment();
+        timeDialog.show(getSupportFragmentManager(), "time_picker");
+    }
+
+    public void onClickDateDialog(View view) {
+        MyDateDialogFragment dateDialog = new MyDateDialogFragment();
+        dateDialog.show(getSupportFragmentManager(), "date_picker");
+    }
+
+    public void onClickProgressDialog(View view) {
+        MyProgressDialogFragment progressDialog = new MyProgressDialogFragment();
+        progressDialog.show(getSupportFragmentManager(), "progress_dialog");
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Toast.makeText(this, "Selected time: " + hourOfDay + ":" + minute,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Toast.makeText(this, "Selected date: " + dayOfMonth + "/" + (month + 1) + "/" +
+                year, Toast.LENGTH_SHORT).show();
     }
 }
